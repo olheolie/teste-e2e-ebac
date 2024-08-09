2cd1fa6c58135f9c57653adc9107f12c3c9011ee
Oliver <biatm.teles@gmail.com>
2024-08-09 02:01:09 -0300

    exercicio e2e

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -69,7 +69,8 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   cy.get('#payment_method_cod').click()
   cy.get('#terms').click()
   cy.get('#place_order').click()
-  });   
   cy.get('.page-title').should('contain', 'PEDIDO RECEBIDO')
+  });   
+
 
 })
